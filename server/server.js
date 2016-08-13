6df19e539caa5ffce93b461afe882eb101e03277
Nur Rony <pro.nmrony@gmail.com>
2016-08-13 09:45:04 +0600

    chore(build): adds nodemon to make dev life easy :wink:

--- server/server.js
+++ server/server.js
@@ -8,5 +8,5 @@ app.get('/*',function (req, res) {
 })
 
 app.listen(3000, function () {
-  console.log('server is listening port 3000')
+  console.log('server is listening port http://localhost:3000')
 })
